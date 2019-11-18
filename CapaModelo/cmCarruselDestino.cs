67f50bf9d63bf1f949d1796ddfadbb87c8119e09
Eduardo <a.066@hotmail.com>
2019-11-17 21:54:19 -0600

    actualización destinos

--- CapaModelo/cmCarruselDestino.cs
+++ CapaModelo/cmCarruselDestino.cs
@@ -8,7 +8,7 @@ namespace CapaModelo
 {
     public class cmCarruselDestino
     {
-
+        public int IdCarrDest { get; set; }
         public int iDestino { get; set; }
         public string sRutaImg { get; set; }
         public string sDescripcion { get; set; }
