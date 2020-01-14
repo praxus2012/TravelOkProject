72939599a3b261442913bfba2fc87f06d3dba27c
Fredy de la Cruz <tfredy02@gmail.com>
2020-01-14 16:11:11 -0600

    Encuesta

--- TravelOKViajes/Scripts/Project/jsEncuesta.js
+++ TravelOKViajes/Scripts/Project/jsEncuesta.js
@@ -1,7 +1,42 @@
-﻿
+﻿var preguntas;
+var respuestas;
+
+$('.pregunta').children().children().click(function (evt) {
+    var numPregunta = parseInt($(this).parent().parent().attr('id').substr(1, 2).trim());
+    var respuesta = parseInt($(this).attr("class").substr(1, 1).trim());
+    console.warn("Pregunta numero: " + numPregunta + " Respuesta a la pregunta: " + respuesta);
+
+    var preguntas;
+    //console.log(respuesta);
+    //allElements = $(numPregunta).add(respuesta);
+    preguntas = $(preguntas).add(numPregunta);
+    respuestas = $(preguntas.respuestas).add(respuesta);
+
+    console.log(preguntas);
+    console.log(respuestas);
+});
+
+$(btnSend).click(function (e) {
+    e.preventDefault();
+    console.log('Enviado');
+    EnviarEncuesta();
+    console.log(allElements);
+});
+
+function SeleccionarDestino() {
+    $(document).on('change', '#selDestinos', function (event) {
+        if ($("#selDestinos option:selected").val() != 0) {
+            console.log($("#selDestinos option:selected").val());
+        }
+    });
+}
+
+
+
 $(document).ready(function() {
     Inicial();
     ObtenerFecha();
+    SeleccionarDestino();
 });
 
 function Inicial() {
@@ -40,4 +75,8 @@ function ObtenerFecha() {
     let today = new Date().toLocaleDateString()
     var lbFecha = document.getElementById('lbFecha');
     lbFecha.innerHTML = today;
+}
+
+function EnviarEncuesta() {
+    console.log($('#urlEnviarEncuesta').val());
 }
