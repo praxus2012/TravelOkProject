e5151f01be629cf41d307f2f0ab29097393c8a49
Eduardo <a.066@hotmail.com>
2020-05-17 11:10:29 -0500

    avance perfil

---
+++ CapaDatos/CD_Perfil.cs
@@ -0,0 +1,28 @@
+﻿using CapaModelo;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaDatos
+{
+    public class CD_Perfil
+    {
+        public List<cmDeudaViajeros> fnlsRecuperaInfo(string sUsuario)
+        {
+            List<cmDeudaViajeros> lsDeudaViaje = new List<cmDeudaViajeros>();
+            try
+            {
+                using (var contexto = new TravelOKEntitiesQA())//QA
+                {
+                    
+                }
+            }catch(Exception ex)
+            {
+                throw ex;
+            }
+            return lsDeudaViaje;
+        }
+    }
+}
---
+++ CapaModelo/cmDeudaViajeros.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaModelo
+{
+    public class cmDeudaViajeros
+    {
+        public int idViaje { get; set; }
+        public string sUsuario { get; set; }
+        public string sDestino { get; set; }
+        public DateTime dtFechaSalida { get; set; }
+        public decimal dCostoViajero { get; set; }
+        public decimal dTotal { get; set; }
+        public decimal dAdeudo { get; set; }
+    }
+}
