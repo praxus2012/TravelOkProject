66c67a8be2ae5158cea48902105e2c7de11822b1
Eduardo <a.066@hotmail.com>
2021-01-11 18:16:27 -0600

    cambios

--- CapaDatos/Administrador/CDA_Destinos.cs
+++ CapaDatos/Administrador/CDA_Destinos.cs
@@ -1,7 +1,9 @@
 ﻿using CapaModelo;
 using System;
 using System.Collections.Generic;
+using System.Configuration;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -17,8 +19,13 @@ namespace CapaDatos.Administrador
             {
                 try
                 {
-                    if (contexto.spiInsertaDestino(CDestino.sDestino, CDestino.sTitulo, CDestino.sSubTit, CDestino.sDescrip,CDestino.sRutaRec,CDestino.sRutaItin,CDestino.sRutaGuia, CDestino.bPrincipal) == -1)
+                    using (var client = new WebClient())
+                    {
+                        client.Credentials = new NetworkCredential(ConfigurationManager.AppSettings["ftpUsr"], ConfigurationManager.AppSettings["ftpPass"]);
+                        client.UploadFile("ftp://"+ ConfigurationManager.AppSettings["ftpHost"] + "/travel/Img/", WebRequestMethods.Ftp.UploadFile, "D:\\Imagenes\\Saved Pictures\\sunset.jpg");
+                    }
 
+                    if (contexto.spiInsertaDestino(CDestino.sDestino, CDestino.sTitulo, CDestino.sSubTit, CDestino.sDescrip,CDestino.sRutaRec,CDestino.sRutaItin,CDestino.sRutaGuia, CDestino.bPrincipal) == -1)
                         return true;
                     return false;
                 }
--- TravelOkAdmin/Controllers/LoginController.cs
+++ TravelOkAdmin/Controllers/LoginController.cs
@@ -1,8 +1,12 @@
 ﻿using CapaDatos;
 using Newtonsoft.Json.Linq;
+using Renci.SshNet;
 using System;
 using System.Collections.Generic;
+using System.Configuration;
+using System.IO;
 using System.Linq;
+using System.Net;
 using System.Web;
 using System.Web.Mvc;
 
@@ -13,8 +17,47 @@ namespace TravelOkAdmin.Controllers
         // GET: Login
         public ActionResult Index()
         {
+            using (var client = new WebClient())
+            {
+                int x = 0;
+                ///FileUploadSFTP(ConfigurationManager.AppSettings["ftpHost"], ConfigurationManager.AppSettings["ftpUsr"], ConfigurationManager.AppSettings["ftpPass"], "D:\\Imagenes\\Saved Pictures\\sunset.jpg");
+                /*client.Credentials = new NetworkCredential(ConfigurationManager.AppSettings["ftpUsr"], ConfigurationManager.AppSettings["ftpPass"]);
+                client.UploadFile("ftp://" + ConfigurationManager.AppSettings["ftpHost"] + "/travel/Img/", WebRequestMethods.Ftp.UploadFile, "D:\\Imagenes\\Saved Pictures\\sunset.jpg");*/
+            }
             return View();
         }
+
+        public void FileUploadSFTP(string sHost, string sUsr, string sPass, string sFile)
+        {
+            var host = sHost;
+            var port = 22;
+            var username = sUsr;
+            var password = sPass;
+
+            // path for file you want to upload
+            var uploadFile = sFile;
+
+            using (var client = new SftpClient(host, port, username, password))
+            {
+                client.Connect();
+                if (client.IsConnected)
+                {
+                    client.ChangeDirectory("/travel/Img");
+                    using (var fileStream = new FileStream(uploadFile, FileMode.Open))
+                    {
+
+                        client.BufferSize = 4 * 1024; // bypass Payload error large files
+                        
+                        client.UploadFile(fileStream, Path.GetFileName(uploadFile));
+                    }
+                }
+                else
+                {
+                    string valor = "";
+                }
+            }
+        }
+
         [HttpPost]
         public ActionResult Logeo(TOA_Usuario datosUsuario)
         {
